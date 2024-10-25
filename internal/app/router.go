58aa18b0d5cc7bf8e160ee072181243f7921eb33
Matthew Pombo <datacorruption10@gmail.com>
2024-10-24 21:32:46 -0700

    removed test and updated example program

--- internal/app/router.go
+++ internal/app/router.go
@@ -14,7 +14,7 @@ type assReq struct {
 
 func disableCacheMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0")
+		w.Header().Set("Cache-Control", "no-cache, must-revalidate, proxy-revalidate, max-age=0")
 		next.ServeHTTP(w, r)
 	})
 }
@@ -30,14 +30,6 @@ func NewRouter() *chi.Mux {
 
 	r.Get("/public/*", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, r.URL.Path[1:])
-	})
-
-	r.Get("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "public/logo-darkmode.png")
-	})
-
-	r.Get("/test", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "public/test.html")
 	})
 
 	// API endpoint to assemble code. Expects a JSON object with a single key "assembly" containing the assembly code.
@@ -66,10 +58,6 @@ func NewRouter() *chi.Mux {
 			"length": length,
 			"binary": numbers,
 		})
-	})
-
-	r.Get("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "public/logo.svg")
 	})
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
--- internal/tailwind.config.js
+++ internal/tailwind.config.js
@@ -6,9 +6,6 @@ module.exports = {
       gridTemplateColumns: {
         '16': 'repeat(16, minmax(0, 1fr))',
       },
-      fontFamily: {
-        ProggyVector: ["ProggyVector"],
-      },
     },
   },
   plugins: [require("@tailwindcss/typography"),require('daisyui')],
